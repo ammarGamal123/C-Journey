2cf0e1c860014ea886057df7180df1e91bec043c
ammarGamal123 <amargamal832909@gmail.com>
2024-11-04 15:19:59 +0200

    learn about how format specifier works

--- Learning_C/main.c
+++ Learning_C/main.c
@@ -36,11 +36,36 @@ void howToDisplayDataTypes() {
     long long int a1 = 1234523663452345;
     printf("%lld\n",a1);
 
+    printf("%lld bytes\n" , sizeof(long long int));
+
 }
+
+void howToFormatSpecifier() {
+    // format specifier % = defines and formats a type of data to be displayed
+    // %c = character
+    // %s = string (array of chars)
+    // %f = float
+
+
+    // %.1 = decimal precision
+    // %1 = minimum field width
+    // %- = left align
+
+    float item1 = 5.75;
+    float item2 = 10.00;
+    float item3 = 100.99;
+
+    printf("Item 1: $%.5f\n" , item1);
+    printf("Item 2: $%8.2f\n" , item2);
+    printf("Item 1: $%-5.2f\n" , item3);
+}
+
+
 int main(void) {
 
-    howToDisplayDataTypes();
+    //howToDisplayDataTypes();
 
+    howToFormatSpecifier();
 
     return 0;
 }
